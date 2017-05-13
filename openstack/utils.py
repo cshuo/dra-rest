6c8366ad03d1dfbb71bd6c21d6758e7c1f7bd428
cshuo <cshuo>
2017-05-14 02:17:01 +0800

    modify resource ontology

--- openstack/utils.py
+++ openstack/utils.py
@@ -35,6 +35,41 @@ def get_token_tenant(request):
     else:
         return {'code':401, 'data':[None, None]}
 
+def get_id_name_maps():
+    token_url = config.AUTH_URL
+    post_d = {'auth': {'tenantName': config.TENANT, 'passwordCredentials': {'username': config.USER, 'password': config.PASSWORD}}}
+    r = requests.post(token_url, json=post_d, headers={'Content-type': 'application/json'})
+    token_id = r.json()["access"]["token"]["id"]
+    tenant_id = r.json()["access"]["token"]["tenant"]["id"]
+    headers = {'Content-type': 'application/json', 'X-Auth-Token': token_id}
+
+    url = config.NOVA_URL + tenant_id + '/servers'
+    r = requests.get(url, headers=headers)
+    rs = {}
+    if r.status_code != 200:
+        return rs
+    vms = r.json()['servers']
+    for v in vms:
+        rs[str(v['name'])] = str(v['id'])
+    return rs
+
+def get_hosts_info():
+    token_url = config.AUTH_URL
+    post_d = {'auth': {'tenantName': config.TENANT, 'passwordCredentials': {'username': config.USER, 'password': config.PASSWORD}}}
+    r = requests.post(token_url, json=post_d, headers={'Content-type': 'application/json'})
+    token_id = r.json()["access"]["token"]["id"]
+    tenant_id = r.json()["access"]["token"]["tenant"]["id"]
+    headers = {'Content-type': 'application/json', 'X-Auth-Token': token_id}
+
+    url = config.NOVA_URL + tenant_id + '/os-hypervisors'
+    r = requests.get(url, headers=headers)
+    rs = {}
+    if r.status_code != 200:
+        return rs
+    hosts = r.json()['hypervisors']
+    for h in hosts:
+        rs[str(h['hypervisor_hostname'])] = h['id']
+    return rs
 
 '''
 the vms/res an app/vm related.
@@ -55,12 +90,21 @@ def get_related(types, **kwargs):
     network.feedFactsToAdd(generateTokenSet(factGraph))
 
     # print closureDeltaGraph.serialize(format='n3')
+    rs = []
     if types == 'res':
         r_list = list(closureDeltaGraph.query('SELECT ?RESULT {:%s :key_res ?RESULT}' % kwargs['app'], initNs=nsMapping))
-    else:
+        for r in r_list:
+            rs.append(str(r.split('#')[1]))
+    elif types == 'app':
         r_list = list(closureDeltaGraph.query('SELECT ?RESULT {:%s :related_to ?RESULT}' % kwargs[types], initNs=nsMapping))
-
-    rs = []
-    for r in r_list:
-        rs.append(str(r.split('#')[1]))
+        id_maps = get_id_name_maps()
+        for r in r_list:
+            name = str(r.split('#')[1])
+            rs.append({'name': name, 'id': id_maps[name]})
+    elif types == 'vm':
+        r_list = list(closureDeltaGraph.query('SELECT ?RESULT {:%s :related_to ?RESULT}' % kwargs[types], initNs=nsMapping))
+        host_ids = get_hosts_info()
+        for r in r_list:
+            name = str(r.split('#')[1])
+            rs.append({'name': name, 'id': host_ids[name]})
     return rs
